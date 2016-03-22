b85ea9cd5c1330425815b71362d4549016cfdff2
khor <khor@anyperk.com>
2016-03-22 13:29:49 +0900

    Init repo with code for linear regression with one feature

---
+++ code/linear_regression_one_feature.py
@@ -0,0 +1,30 @@
+# Model linear regression y = Wx + b
+x = tf.placeholder(tf.float32, [None, 1])
+W = tf.Variable(tf.zeros([1,1]))
+b = tf.Variable(tf.zeros([1]))
+product = tf.matmul(x,W)
+y = product + b
+y_ = tf.placeholder(tf.float32, [None, 1])
+
+# Cost function sum((y_-y)**2)
+cost = tf.reduce_sum(tf.pow((y_-y), 2))
+
+# Training using Gradient Descent to minimize cost
+train_step = tf.train.GradientDescentOptimizer(0.00001).minimize(cost)
+
+sess = tf.Session()
+init = tf.initialize_all_variables()
+sess.run(init)
+steps = 100 
+for i in range(steps):
+  # Create fake data for y = W.x + b where W = 2, b = 0
+  xs = np.array([[i]])
+  ys = np.array([[2*i]])
+  # Train
+  feed = { x: xs, y_: ys }
+  sess.run(train_step, feed_dict=feed)
+  print("After %d iteration:" % i)
+  print("W: %f" % sess.run(W))
+  print("b: %f" % sess.run(b))
+
+# NOTE: W should be close to 2, and b should be close to 0
