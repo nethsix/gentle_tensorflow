f0b992b0b8baaa84fce8ca22fb865c562bafa83b
khor <khor@anyperk.com>
2016-05-14 13:02:38 +0900

    Add CUSOMIZABLE section, and names for tensorboard
    * Add tf.Variable, tf.placeholder names for tensorboard usg
    * Move variables that are customizable, e.g., batch_size, steps, etc.,
      into CUSTOMIZABLE section

--- code/linear_regression_one_feature_using_mini_batch_with_tensorboard.py
+++ code/linear_regression_one_feature_using_mini_batch_with_tensorboard.py
@@ -1,10 +1,18 @@
 import numpy as np
 import tensorflow as tf
 
+# CUSTOMIZABLE: Collect/Prepare data
+datapoint_size = 1000
+batch_size = 1
+steps = 10000
+actual_b = 10
+learn_rate = 0.0000001
+log_file = "/tmp/mnist_batch_1_with_b10_big_xes_grad_desc_logs_20160425b"
+
 # Model linear regression y = Wx + b
-x = tf.placeholder(tf.float32, [None, 1])
-W = tf.Variable(tf.zeros([1,1]))
-b = tf.Variable(tf.zeros([1]))
+x = tf.placeholder(tf.float32, [None, 1], name="x")
+W = tf.Variable(tf.zeros([1,1]), name="W")
+b = tf.Variable(tf.zeros([1]), name="b")
 with tf.name_scope("Wx_b") as scope:
   product = tf.matmul(x,W)
   y = product + b
@@ -14,7 +22,7 @@ W_hist = tf.histogram_summary("weights", W)
 b_hist = tf.histogram_summary("biases", b)
 y_hist = tf.histogram_summary("y", y)
 
-y_ = tf.placeholder(tf.float32, [None, 1])
+y_ = tf.placeholder(tf.float32, [None, 1], name="y_")
 
 # Cost function sum((y_-y)**2)
 with tf.name_scope("cost") as scope:
@@ -23,19 +31,14 @@ with tf.name_scope("cost") as scope:
 
 # Training using Gradient Descent to minimize cost
 with tf.name_scope("train") as scope:
-  train_step = tf.train.GradientDescentOptimizer(0.0000000001).minimize(cost)
-
-# Collect/Prepare data
-datapoint_size = 1000
-batch_size = 1000
-steps = 1000
+  train_step = tf.train.GradientDescentOptimizer(learn_rate).minimize(cost)
 
 all_xs = []
 all_ys = []
 for i in range(datapoint_size):
-  # Create fake data for y = W.x + b where W = 2, b = 0
-  all_xs.append(i)
-  all_ys.append(2*i)
+  # Create fake data for y = W.x + b where W = 2, b = actual_b
+  all_xs.append(i%10)
+  all_ys.append(2*(i%10)+actual_b)
 
 all_xs = np.transpose([all_xs])
 all_ys = np.transpose([all_ys])
@@ -44,7 +47,7 @@ sess = tf.Session()
 
 # Merge all the summaries and write them out to /tmp/mnist_logs
 merged = tf.merge_all_summaries()
-writer = tf.train.SummaryWriter("/tmp/mnist_batch_full_grad_desc_logs_20160404", sess.graph_def)
+writer = tf.train.SummaryWriter(log_file, sess.graph_def)
 
 init = tf.initialize_all_variables()
 sess.run(init)
@@ -69,7 +72,9 @@ for i in range(steps):
   else:
     feed = { x: xs, y_: ys }
     sess.run(train_step, feed_dict=feed)
-    #  print("cost: %f" % sess.run(cost, feed_dict=feed))
+    print("y: %s" % sess.run(y, feed_dict=feed))
+    print("y_: %s" % ys)
+    print("cost: %f" % sess.run(cost, feed_dict=feed))
   print("After %d iteration:" % i)
   print("W: %f" % sess.run(W))
   print("b: %f" % sess.run(b))
